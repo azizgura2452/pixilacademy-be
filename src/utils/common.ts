8fdcc77c29c2ad24ce20be241f3ffac1ad97120b
azizgura2452 <azizgura@gmail.com>
2025-04-21 07:01:29 +0300

    image consoling

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,9 +3,10 @@ const API_URL = process.env.API_URL
 
 export const getImageSrc = (url: string) => {
   if (!url || typeof url !== 'string') return PLACEHOLDER_IMAGE
-
+  console.log(6, API_URL, url)
   if (url && typeof url === 'string') {
     // Check if it's already a full URL
+    console.log(9, API_URL, url)
     return url.startsWith('http') ? url : `${API_URL}${url}`
   }
 
